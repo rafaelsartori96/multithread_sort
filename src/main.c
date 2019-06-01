199ec6136640789848cbad8a9550778605963d89
rafaelsartori96 <rafael.sartori96@gmail.com>
2019-06-01 10:38:28 -0300

    Começando desenvolvimento

--- src/main.c
+++ src/main.c
@@ -1,17 +1,50 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/**
+ * Ordenador multithread
+ * 
+ * Rafael Sartori M. Santos, 186154
  */
-
 #include <stdio.h>
 
+
+typedef struct {
+    int tamanho;
+    int capacidade;
+    int *vetor;
+} vetor_t;
+
+
+void inicializar(vetor_t *v) {
+    v->tamanho = 0;
+    v->capacidade = CAPACIDADE_INICIAL;
+    v->vetor = malloc(v->capacidade * sizeof(int));
+    if (v->vetor == NULL) {
+        fprintf(stderr, "Falha ao alocar vetor inicial! Saindo...\n");
+        exit(1);
+    }
+}
+
+
+void expandir(vetor_t *v) {
+    // realocamos com o dobro de tamanho
+    v->capacidade *= 2;
+    int *novo_vetor = malloc(v->capacidade * sizeof(int));
+    if (novo_vetor == NULL) {
+        fprintf(stderr, "Falha ao realocar vetor! Saindo...\n");
+        exit(1);
+    }
+
+    // passamos todos os itens do vetor
+    for (int i = 0; i < v->tamanho; i++) {
+        novo_vetor[i] = v->vetor[i];
+    }
+
+    // substituímos na estrutura
+    free(v->vetor);
+    v->vetor = novo_vetor;
+}
+
+
 int main() {
-
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+    vetor_t v;
+    inicializar(&v);
 }
